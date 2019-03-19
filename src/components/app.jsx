ce39db1ba251a56833aae1b818e2daccc1349471
Aliaksandr Pashynski <aliaksandr_pashynski@epam.com>
2019-03-19 09:52:58 +0300

    redux added

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,27 +1,44 @@
 import React, {Component} from 'react';
+import { createStore } from 'redux';
 import TodoSection from './todo-section'
+import uniqid from 'uniqid'
 
-const items = [
-    {
-        id: 0,
-        title: 'Learn react',
-        completed: false
-    },
-    {
-        id: 1,
-        title: 'Learn javasctipt',
-        completed: true
-    },
-    {
-        id: 2,
-        title: 'Become front-end guru',
-        completed: false
+const initialState = {
+    items: [
+        {
+            id: 0,
+            title: 'Learn react',
+            completed: false
+        }
+    ]
+};
+
+function reducer(state, action) {
+    if (action.type === 'ADD_TODO') {
+        return {
+            items: state.items.concat(
+                {
+                    id: uniqid(),
+                    title: action.title,
+                    completed: false
+                }
+            ),
+        };
+    } else {
+        return state;
     }
-]
+}
+
+const store = createStore(reducer, initialState);
 
 class App extends Component {
+
+    componentDidMount() {
+        store.subscribe(() => this.forceUpdate());
+    }
+
     render() {
-        return <TodoSection items={items}/>
+        return <TodoSection items={initialState.items}/>
     }
 }
 
